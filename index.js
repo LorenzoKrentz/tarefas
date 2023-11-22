9c6e7bac2fe81fec0c9f270cafc6f47b483a527a
LorenzoKrentz <137554742+LorenzoKrentz@users.noreply.github.com>
2023-11-22 16:00:19 -0300

    commit changes
    commit

---
+++ index.js
@@ -0,0 +1,39 @@
+//Questão 1
+let = nome
+let = cidade
+
+console.log(typeof nome)
+console.log(typeof cidade)
+
+nome = prompt('Digite seu nome:')
+cidade = prompt('Digite a cidade onde você mora:')
+
+console.log(typeof nome)
+console.log(typeof cidade)
+
+console.log('Olá, meu nome é ${nome} e eu moro em ${cidade}.')
+
+//Questão 2
+let = pergunta1
+let = pergunta2
+let = pergunta3
+
+pergunta1 = prompt("Você gosta de programar? (Responda com Sim ou Não)");
+pergunta2 = prompt("Você já viajou para fora do país? (Responda com Sim ou Não)");
+pergunta3 = prompt("Você tem animais de estimação? (Responda com Sim ou Não)");
+
+console.log("Você gosta de programar? Resposta:", pergunta1)
+console.log("Você já viajou para fora do país? Resposta:", pergunta2)
+console.log("Você tem animais de estimação? Resposta:", pergunta3)
+
+//Questão 3
+let x = 5 
+let y = 10
+let z 
+
+z = x
+x = y
+y = z
+
+console.log("Troquei valores x = 10", x)
+console.log("Troquei valores y = 5", y)
